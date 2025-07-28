6b7c417166d5cb58d47dd6e1b194f723a43378aa
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-07-28 17:19:04 +0200

    Added a countingSort function adapted for use within the radix sort algorithm. This function sorts elements based on the digit at a given exponent (exp), ensuring stability for each sorting pass.

--- radix-sort.c
+++ radix-sort.c
@@ -11,3 +11,32 @@ int getMax(int arr[], int n)
 
 }
 
+// Counting sort function adapted for Radix
+void countingSort(int arr[], int n, int exp)
+{
+    int output[n];
+    int count[10] = {0};
+
+    // Counts digit occurrences
+    for (int i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+
+    // Calculates actual positions
+    for (int i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+
+    // Build stable output array
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int idx = (arr[i] / exp) % 10;
+
+        output[count[idx] - 1] = arr[i];
+        count[idx]--;
+    }
+
+    // Copy back
+    for (int i = 0; i < n; i++)
+        arr[i] = output[i];
+
+    
+}
